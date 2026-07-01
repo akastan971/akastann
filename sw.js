049ce61a5b0b31b440b152997281f335fa959224
A KA STANN & DEDETTE <akastan971@gmail.com>
2026-07-01 15:24:17 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'akastann-v1';
+const urlsToCache = [
+  '/akastann/landing.html',
+  '/akastann/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
